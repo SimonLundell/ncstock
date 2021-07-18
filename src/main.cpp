5211c72f96d7c7f97a9a2ba56f919478ce6cc144
SimonLundell <simonlundell@hotmail.com>
2021-07-18 12:14:50 +0200

    Get terminal size

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,43 @@
 #include <ncurses.h>
 #include <curses.h>
 
-#define LINES 25
-#define COLS 100
-
 int main() 
 {
+    /*
+        \* Inititialization of ncurses window
+    */
     initscr();                 /* Start curses mode     */
     refresh();  /* Part of init phase, mandatory */
-    int row; int col = 1;
+    int ch;
 
-    WINDOW* win = newwin(LINES,COLS,0,0); /* Create a window with lines, cols and start coords */
+    int ROWS; 
+    int COLS;
+    getmaxyx(stdscr, ROWS, COLS); /* get size of stdscr (terminal) and return to ROWS, COLS */
 
-    wmove(win, row, col); /* Moves cursor position? */
+    WINDOW* win = newwin(ROWS,COLS,0,0); /* Create a window with lines, cols and start coords */
+
+    //wmove(win, 1, 1); /* Moves cursor position? */
     wborder(win, 0, 0, 0, 0, 0, 0, 0, 0); /* Create a border, if 0 it will take defaults */
-    wprintw(win, "Hello mate!"); /* Print something to the window */
+    mvwprintw(win, 1, 1, "Hello mate!"); /* Move cursor to 1,1 and print something to the window */
     wrefresh(win); /* Refresh the window, mandatory when changes occur */
-    cbreak(); /* Break on key? */
+    cbreak(); /* Disable line buffer (raw() works similar) */
     noecho(); /* Dont show key on terminal */
+    keypad(stdscr, TRUE); /* Enable keypad input (F-keys, arrows etc.) */
+
+    
+
     while (true)
     {
-        if ((getch()) != ERR)
+        ch = getch();
+        if (ch != ERR)
         {
-            std::cout << "Jey!";
-            row += 1;
-            wmove(win, row, col);
-            refresh();
+            if ((wprintw(win,"%s","Now I'm here")) == ERR)
+            {
+                std::cout << "Error";
+            }
+            
         }
+        wrefresh(win);
     }
 
     endwin();                  /* End curses mode    */
