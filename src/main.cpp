faaa7d5eb46e0c508f21182e221e87804ea75a01
SimonLundell <simonlundell@hotmail.com>
2021-07-20 20:27:49 +0200

    underline. print whatever you want. Solve stdscr / win

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,14 @@
 #include <ncurses.h>
 #include <curses.h>
 
+int ROWS, COLS;
+
 enum attrib {quit, 
-            bold, 
+            bold,
+            underline,
             unbold};
 
-std::map<std::string, attrib> queries{{"quit", quit},{"bold", bold},{"clear", unbold}};
+std::map<std::string, attrib> queries{{"quit", quit},{"bold", bold},{"underline", underline},{"clear", unbold}};
 
 int hashtable(const std::string &inp) {
     std::map<std::string, attrib>::iterator it;
@@ -27,42 +30,42 @@ std::string getString()
     {
         msg.push_back(n); /* Push back character from getch() to msg while it is not newline */ 
         n = getch();
+        if (n == KEY_RESIZE)
+        {
+            std::cout << "Resizing...";
+        }
     }
     return msg;
 }
 
 int main() 
 {
+    int ch, x, y;
     /*
         \* Inititialization of ncurses window
     */
     initscr();                 /* Start curses mode     */
     refresh();  /* Part of init phase, mandatory */
-    int ch, ROWS, COLS, x, y;
+    
+    /* stdscr inits */
     getmaxyx(stdscr, ROWS, COLS); /* get size of stdscr (terminal) and return to ROWS, COLS */
+    mvwprintw(stdscr, 0, 0, "Quit: quit. Bold text: bold. Clear attributes: clear");
+    wrefresh(stdscr);
 
-    WINDOW* win = newwin(ROWS,COLS,0,0); /* Create a window with lines, cols and start coords */
-
-    //wmove(win, 1, 1); /* Moves cursor position? */
+    /* Window inits */
+    WINDOW* win = newwin(ROWS-1,COLS,1,0); /* Create a window with lines, cols and start coords */
+    wmove(win, 1, 1); /* Moves cursor position? */
     wborder(win, 0, 0, 0, 0, 0, 0, 0, 0); /* Create a border, if 0 it will take defaults */
-    mvwprintw(win, 1, 1, "Hello mate!"); /* Move cursor to 1,1 and print something to the window */
     wrefresh(win); /* Refresh the window, mandatory when changes occur */
+
     cbreak(); /* Disable line buffer (raw() works similar) */
     noecho(); /* Dont show key on terminal */
     keypad(stdscr, TRUE); /* Enable keypad input (F-keys, arrows etc.) */
     
     while (true)
     {
-        if (ch != ERR)
-        {
-            getyx(win,y,x);
-            wmove(win,y+1,1);
-            if ((wprintw(win,"%s","Now I'm here")) == ERR)
-            {
-                std::cout << "Error";
-            }
-        }
-        switch (hashtable(getString()))
+        std::string messageToPrint = getString();
+        switch (hashtable(messageToPrint))
         {
             case(0):
                 goto endloop;
@@ -70,8 +73,22 @@ int main()
                 wattron(win, A_BOLD);
                 break;
             case(2):
-                wattroff(win, A_BOLD);
+                wattron(win, A_UNDERLINE);
                 break;
+            case(3):
+                wattroff(win, A_BOLD);
+                wattroff(win, A_UNDERLINE);
+                break;
+        }
+
+        if (ch != ERR)
+        {
+            getyx(win,y,x);
+            wmove(win,y+1,1);
+            if ((wprintw(win,"%s", messageToPrint.c_str())) == ERR)
+            {
+                std::cout << "Error";
+            }
         }
 
         wrefresh(win);
