163a2a85303f2b23043feea34355efe7d540cfe0
SimonLundell <simonlundell@hotmail.com>
2021-10-02 09:36:05 +0200

    Working API call!

--- include/Trading.hpp
+++ include/Trading.hpp
@@ -5,15 +5,24 @@
 #include <unordered_map>
 #include <iostream>
 #include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/Net/HTTPSClientSession.h>
+#include <Poco/StreamCopier.h>
+#include <Poco/Path.h>
+#include <Poco/URI.h>
 
 class TradingInfo
 {
     public:
+        TradingInfo();
         void callAPI(const std::string asset);
         std::string getBTC();
         [[maybe_unused]]std::string getAFRY();
     private:
         std::unordered_map<std::string, int> assets;
         const std::string BTC = "{'Realtime Currency Exchange Rate': {'1. From_Currency Code': 'BTC', '2. From_Currency Name': 'Bitcoin', '3. To_Currency Code': 'USD', '4. To_Currency Name': 'United States Dollar', '5. Exchange Rate': '45703.08000000', '6. Last Refreshed': '2021-08-15 17:06:02', '7. Time Zone': 'UTC', '8. Bid Price': '45703.07000000', '9. Ask Price': '45703.08000000'}}"; 
+
+        Poco::URI* _uri;
+        std::string _path;
 };
 #endif
--- src/Trading.cpp
+++ src/Trading.cpp
@@ -1,17 +1,32 @@
 #include "../include/Trading.hpp"
 
+TradingInfo::TradingInfo()
+{
+}
+
 void TradingInfo::callAPI(const std::string asset)
 {
-    auto asset_info = Poco::Net::HTTPRequest("https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=4PC87FQ16E7YV46Q");
-    std::cout << asset_info.size() << "\n";   
-    assets.emplace("Bitcoin",45000);
+    _path = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=4PC87FQ16E7YV46Q";
+    auto uri = Poco::URI(_path);
+    auto path = uri.getPathAndQuery();
+    auto _session = Poco::Net::HTTPSClientSession(uri.getHost(),uri.getPort());
+
+    auto asset_info = Poco::Net::HTTPRequest(Poco::Net::HTTPRequest::HTTP_GET, 
+                                            path,
+                                            Poco::Net::HTTPMessage::HTTP_1_1);
+    _session.sendRequest(asset_info);
+    Poco::Net::HTTPResponse res;
+    //std::cout << res.getStatus() << " " << res.getReason() << "\n";
+
+    std::istream &is = _session.receiveResponse(res);
+    Poco::StreamCopier::copyStream(is, std::cout);
 }
 std::string TradingInfo::getBTC()
 {
-    auto it = assets.find("Bitcoin");
-    std::string ret = it->first + std::to_string(it->second);
+    //auto it = assets.find("Bitcoin");
+    //std::string ret = it->first + std::to_string(it->second);
 
-    return ret;
+    return "Bitcoin";
 }
 [[maybe_unused]]std::string TradingInfo::getAFRY()
 {
